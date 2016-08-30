f38705061a27b839131ee93486284e5c98151c84
Brendan Carne <bjcarne@gmail.com>
2016-08-30 13:52:39 +1000

    major redesign to be completely event driven

--- socket_transceiver.py
+++ socket_transceiver.py
@@ -1,119 +1,119 @@
 #!/usr/bin/env python3
 
-""" A socket interface designed to take IP payloads and transmit them to another LoRa """
-#
-# add some licence 
-#
+""" An asynchronous socket <-> LoRa interface """
 
-import sys
-from socket import socket
-from time import sleep
+# MIT License
+#
+# Copyright (c) 2016 bjcarne
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import sys, asyncore
 from SX127x.LoRa import *
 from SX127x.board_config import BOARD
 
 BOARD.setup()
 
-class LoRaSocket(LoRa):
+class Server(asyncore.dispatcher):
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket()
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(1) 
+        
+    def handle_accepted(self, sock, addr):
+        print("Connection from %s:%s" % sock.getpeername())
+        self.conn = Handler(sock)
 
+class Handler(asyncore.dispatcher):
+    def __init__(self, sock):
+        asyncore.dispatcher.__init__(self, sock)
+        self.databuffer = b""
+        self.tx_wait = 0
+    
+    # when data is available on socket send to LoRa
+    def handle_read(self):
+        if not self.tx_wait:
+            data = self.recv(127)
+            print('Send:' + str(data))
+            lora.write_payload(list(data))
+            lora.set_dio_mapping([1,0,0,0,0,0]) # set DIO0 for txdone
+            lora.set_mode(MODE.TX)
+            self.tx_wait = 1 
+    
+    # when data for the socket, send
+    def handle_write(self):
+        if self.databuffer:
+            self.send(self.databuffer)
+            self.databuffer = b""
+            
+    def handle_close(self):
+        print("Client disconnected")
+        self.close()
+        
+class LoRaSocket(LoRa):
+    
     def __init__(self, verbose=False):
         super(LoRaSocket, self).__init__(verbose)
         self.set_mode(MODE.SLEEP)
+        self.set_pa_config(pa_select=1)
+        self.set_max_payload_length(128) # set max payload to max fifo buffer length
         self.set_dio_mapping([0] * 6) #initialise DIO0 for rxdone  
         
-    def create_socket(self):
-        host = "127.0.0.1"
-        port = 20000
-         
-        mySocket = socket()
-        mySocket.bind((host,port))
-         
-        mySocket.listen(1)
-        print("Waiting for socket connection")
-        
-        conn, addr = mySocket.accept()
-        print ("Connection from: " + str(addr))        
-        
-        return conn
-
-    def on_rx_done(self):
-        self.clear_irq_flags(RxDone=1) # clear rxdone IRQ flag
-        
+    # when LoRa receives data send to socket conn
+    def on_rx_done(self):        
         payload = self.read_payload(nocheck=True) #get payload from lora fifo
-        print('Payload received:' + str(bytes(payload)))
+        print('Recv:' + str(bytes(payload)))
         
-        conn.send(bytes(payload)) #send to socket conn
+        server.conn.databuffer = bytes(payload) #send to socket conn
         
+        self.clear_irq_flags(RxDone=1) # clear rxdone IRQ flag
         self.reset_ptr_rx()
         self.set_mode(MODE.RXCONT)
-
-
-    def on_tx_done(self):
-        print('\ntxdone')
-        self.clear_irq_flags(TxDone=1) # clear txdone IRQ flag
-        self.set_dio_mapping([0] * 6) #DIO0 set for rxdone    
-        self.set_mode(MODE.RXCONT)
-
-    def on_cad_done(self):
-        print("\non_CadDone")
-        print(self.get_irq_flags())
-
-    def on_rx_timeout(self):
-        print("\non_RxTimeout")
-        print(self.get_irq_flags())
-
-    def on_valid_header(self):
-        print("\non_ValidHeader")
-        print(self.get_irq_flags())    
-
-    def on_payload_crc_error(self):
-        print("\non_PayloadCrcError")
-        print(self.get_irq_flags())
-
-    def on_fhss_change_channel(self):
-        print("\non_FhssChangeChannel")
-        print(self.get_irq_flags())
-
-    def start(self):
-        self.set_max_payload_length(128) # set max payload to max fifo buffer length
-        self.reset_ptr_rx() 
-        self.set_mode(MODE.RXCONT) # stay in RXCONT unless we have data to transmit 
-        
-        while True:
-                data = conn.recv(128) # wait for data on socket
-                if not data:
-                    break
-                        
-                # send data to LoRa
-                self.set_dio_mapping([1,0,0,0,0,0]) # set DIO0 for txdone
-                self.write_payload(list(data)) 
-                self.set_mode(MODE.TX)
     
+    # after data sent by LoRa reset to receive mode
+    def on_tx_done(self):
+        self.clear_irq_flags(TxDone=1) # clear txdone IRQ flag
+        self.set_dio_mapping([0] * 6)    
+
+        self.set_mode(MODE.RXCONT)
+        server.conn.tx_wait = 0
+   
 
 if __name__ == '__main__':
     
-    lora = LoRaSocket(verbose=True)
-
-    lora.set_pa_config(pa_select=1)
+    server = Server('localhost', 20000)
+    
+    lora = LoRaSocket(verbose=False)
 
     print(lora)
     
-    assert(lora.get_agc_auto_on() == 1)
-
-    try: input("Press enter to start...")
-    except: pass
-
     try:
-        conn = lora.create_socket() # set up socket connection
-        lora.start()
+        asyncore.loop()
+
     except KeyboardInterrupt:
-        sys.stdout.flush()
-        print("")
-        sys.stderr.write("KeyboardInterrupt\n")
+        sys.stderr.write("\nKeyboardInterrupt\n")
+        
     finally:
-        sys.stdout.flush()
-        print("")
         lora.set_mode(MODE.SLEEP)
-        print(lora)
         print("Closing socket connection")
-        conn.close()
+        server.close()
         BOARD.teardown()
